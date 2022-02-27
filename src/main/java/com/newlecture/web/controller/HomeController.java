b7e39245a0f987ffaa980b36c6f5962b3761d9c5
한성현 <hansung5281@naver.com>
2022-02-27 21:26:54 +0900

    #10 젠킨스 종료 테스트

--- src/main/java/com/newlecture/web/controller/HomeController.java
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 	@RequestMapping("/index")
 	public String asdf() {
 		System.out.println("income");
-		return "Hello Sung~! 변경99";
+		return "Hello Sung~! 변경1010";
 	}
 	@RequestMapping("/index2")
 	public String aaa() {
